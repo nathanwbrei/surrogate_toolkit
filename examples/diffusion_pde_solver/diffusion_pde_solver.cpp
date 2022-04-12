0565ff63df5cd4e5db909f640cd3a04778145b10
Nathan Brei <nbrei@jlab.org>
2022-04-12 01:24:16 -0400

    Rough cut of diffusion solver

--- examples/diffusion_pde_solver/diffusion_pde_solver.cpp
+++ examples/diffusion_pde_solver/diffusion_pde_solver.cpp
@@ -45,6 +45,51 @@ void print_matrix(std::ostream& os, float* arr, int nrows, int ncols) {
     os << std::endl;
 }
 
+/// Uses Gauss-Seidel iteration with a second-order central finite difference discretization.
+/// Assume homogeneous Dirichlet boundary conditions for simplicity for now.
+/// Parameters:     temperature_matrix
+///                 nrows
+///                 ncols
+
+
+int solve_stationary_heat_eqn(float* temperature_matrix, int nrows, int ncols,
+                               std::function<float(float, float)> forcing_fn
+                               ) {
+
+    float* T = temperature_matrix;
+    double error_threshold = 0.0001;
+    double residual = 2*error_threshold;
+    double hx = 1.0/(ncols+1);
+    double hy = 1.0/(nrows+1);
+    double wx = 1.0/(hx * hx);
+    double wy = 1.0/(hy * hy);
+    double wh = 2*(wx+wy);   // Usually 4/(h^2)
+    int iters = 0;
+
+    while (residual > error_threshold) {
+        for (int r=1; r<nrows-1; ++r) {
+            for (int c=1; c<ncols-1; ++c) {
+                // T[r,c] = -forcing_fn(r,c)/wh + wx*(T[r,c-1] + T[r,c+1]) + wy*(T[r-1,c] + T[r+1,c]);
+                T[r*ncols+c] = -forcing_fn(r,c)/wh + wx*(T[r*ncols+c-1] + T[r*ncols+c+1]) + wy*(T[(r-1)*ncols+c] + T[(r+1)*ncols+c]);
+            }
+        }
+        print_matrix(std::cout, temperature_matrix, nrows, ncols);
+        iters++;
+        residual = 0;
+        double cell_residual = 0;
+        double sum_of_cell_residuals_squared = 0;
+        for (int r=1; r<nrows-1; ++r) {
+            for (int c=1; c<ncols-1; ++c) {
+                cell_residual = -forcing_fn(r,c) - wh*T[r*ncols+c] + wx*(T[r*ncols+c-1] + T[r*ncols+c+1]) + wy*(T[(r-1)*ncols+c] + T[(r+1)*ncols+c]);
+                sum_of_cell_residuals_squared += cell_residual*cell_residual;
+            }
+        }
+        residual = sqrt((1.0/(nrows+ncols)) * sum_of_cell_residuals_squared);
+    }
+    return iters;
+}
+
+
 void fill_matrix(float* arr, int nrows, int ncols) {
     for (int row=0; row<nrows; ++row) {
         for (int col=0; col<ncols; ++col) {
@@ -54,7 +99,20 @@ void fill_matrix(float* arr, int nrows, int ncols) {
     }
 }
 
+void zero_matrix(float* arr, int nrows, int ncols) {
+    for (int row=0; row<nrows; ++row) {
+        for (int col=0; col<ncols; ++col) {
+            size_t idx = row*ncols + col;
+            arr[idx] = 0;
+        }
+    }
+}
+
+#define PI 3.141592
+
+
 int main() {
+    /*
     float matrix[] = { 1,  2,  3,  4,  5,
                        6,  7,  8,  9, 10,
                       11, 12, 13, 14, 15,
@@ -91,9 +149,17 @@ int main() {
     model->train_from_captures();
 
     fill_matrix(matrix, 4, 5);
-    model->infer(surrogate);
+    surrogate.call_model();
 
     std::cout << "Buffer after running the surrogate model:" << std::endl;
     print_matrix(std::cout, matrix, 4, 5);
+
+     */
+    float temperature[10*10];
+    zero_matrix(temperature, 10, 10);
+    auto forcing_fn = [](float x, float y) {return -2*PI*PI*sin(PI*x)*sin(PI*y);};
+    int iters = solve_stationary_heat_eqn(temperature, 10, 10, forcing_fn);
+    std::cout << "Calculated solution in " << iters << " iters." << std::endl;
+    print_matrix(std::cout, temperature, 10, 10);
 }
 
