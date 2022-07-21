278ca34473f97f5611b4d878e2a0c63e47d72c44
Colin Wolfe <Coho905@gmail.com>
2022-07-21 15:40:16 -0400

    Using the PyTorch Neural network instead of the feed forward neural network

---
+++ examples/tutorial/pytorch_tutorial.cpp
@@ -0,0 +1,33 @@
+
+// Copyright 2021, Jefferson Science Associates, LLC.
+// Subject to the terms in the LICENSE file found in the top-level directory.
+
+#include <iostream>
+#include <iomanip>
+#include <model.h>
+#include <surrogate_builder.h>
+#include "feedforward_model.h"
+#include "torchscript_model.h"
+
+
+phasm::TorchscriptModel model = phasm::TorchscriptModel("~/home/USER/phasm/python/tutorial.pt"); // update PATH depending on the user
+
+
+phasm::Surrogate s_surrogate = phasm::SurrogateBuilder()
+        .set_model(std::make_shared<phasm::TorchscriptModel>(phasm::TorchscriptModel("~/home/coho905/phasm/python/tutorial.pt")))
+        .local_primitive<double>("x", phasm::IN)
+        .local_primitive<double>("y", phasm::IN)
+        .local_primitive<double>("z", phasm::IN)
+        .local_primitive<double>("result", phasm::OUT)
+        .finish();
+
+
+
+
+
+
+int main(int argc, char** argv) {
+
+    std::cout<< "testing the implementation";
+    return 0;
+}
