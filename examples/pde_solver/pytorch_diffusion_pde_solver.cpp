100255a91346088784172b3257fe0f7a3a151ce8
Dhruv Bejugam <73863691+dhruvsbe@users.noreply.github.com>
2022-07-28 11:55:11 -0400

    Starting C++ Implementation For PyTorch PDE Solver
    Wasn't able to get as much done as I'd like, but a little start to C++ implementation of heat diffusion PDE solver.

---
+++ examples/pde_solver/pytorch_diffusion_pde_solver.cpp
@@ -0,0 +1,19 @@
+
+// Copyright 2021, Jefferson Science Associates, LLC.
+// Subject to the terms in the LICENSE file found in the top-level directory.
+
+#include <iostream>
+#include <iomanip>
+#include <model.h>
+#include <surrogate_builder.h>
+#include "feedforward_model.h"
+#include "torchscript_model.h"
+
+phasm::TorchscriptModel PDE_Solver = phasm::TorchscriptModel("~/phasm/python/PDE_Solver.pt");
+
+phasm::Surrogate s_surrogate = phasm::SurrogateBuilder()
+    .set_model(std::make_shared<phasm::TorchscriptModel>(phasm::TorchscriptModel("~/phasm/python/PDE_Solver.pt")))
+
+int main() {
+    
+}
