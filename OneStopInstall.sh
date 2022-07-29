9c6da406ea018f44cadfaf97637a123f44c5a375
Colin Wolfe <Coho905@gmail.com>
2022-07-29 16:35:03 -0400

    updating the one stop installation shell

--- OneStopInstall.sh
+++ OneStopInstall.sh
@@ -10,9 +10,13 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 
 ./download_deps.sh
+
 export DEPS=`pwd`/deps
+
 mkdir build
 cd build
+
+
 cmake -DCMAKE_PREFIX_PATH="$DEPS/libtorch;$DEPS/JANA2/install" -DLIBDWARF_DIR="$DEPS/libdwarf-0.3.4/installdir" -DPIN_ROOT="$DEPS/pin" ..
 make install
 
