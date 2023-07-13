d9df1b10c644360598f280f0b57f45866cb04311
Nathan Brei <nbrei@jlab.org>
2023-07-12 20:04:54 -0400

    Set up test case with scalar-valued output

--- julia_plugin/test/julia_tests.cc
+++ julia_plugin/test/julia_tests.cc
@@ -5,13 +5,35 @@
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 #include <julia_model.h>
+#include <surrogate_builder.h>
 
-TEST_CASE("Hello world") {
+
+namespace phasm_julia_tests {
+
+double square(double x) { return x * x; }
+
+TEST_CASE("Scalar-valued result") {
     
-    phasm::JuliaModel model("TestModel.jl");
-    model.initialize();
-    //model.add_model_vars()
-    
-    REQUIRE(true == true);
+    auto model = std::make_shared<phasm::JuliaModel>("TestModel.jl");
+
+    phasm::Surrogate f_surrogate = phasm::SurrogateBuilder()
+        .set_model(model)
+        .local_primitive<double>("x", phasm::IN)
+        .local_primitive<double>("f", phasm::OUT)
+        .finish();
+
+
+    double x = 2.0;
+    double f = 0.0;
+    std::cout << "Before call_model(): f = " << f << std::endl;
+
+    f_surrogate
+        .bind_original_function([&](){ f = square(x); })
+        .bind_all_callsite_vars(&x, &f)
+        .call_model();
+
+    std::cout << "After call_model(): f = " << f << std::endl;
+    REQUIRE(f != 0.0);
 }
 
+} // namespace phasm_julia_tests
