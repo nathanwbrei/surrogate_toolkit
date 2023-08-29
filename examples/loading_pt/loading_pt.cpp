783c6bba377609910b35912eefefd33ee60f41ee
Xinxin Mei <xmei@jlab.org>
2023-08-29 13:57:35 -0400

    Add benchmarking function
    Test on CPU side
    
    ```
    Benchmarking on CPU
    batch_size,     time (milli-seconds)
    64,     6.227
    128,    3.867
    256,    5.437
    512,    11.13
    1024,   19.229
    2048,   35.676
    4096,   80.57
    8192,   146.265
    16384,  329.014
    ```

--- examples/loading_pt/loading_pt.cpp
+++ examples/loading_pt/loading_pt.cpp
@@ -15,9 +15,36 @@
 
 #include <iostream>
 #include <memory>
+#include <chrono>
+#include <cmath>
 
-# define BATCH_SIZE 2048
-# define SEQ_LENGTH 7  // For the test lstm_model.pt model only
+# define MIN_BATCH_SIZE 64
+# define MAX_BATCH_SIZE 16384
+# define DEMO_BATCH_SIZE 2048
+# define DEMO_SEQ_LENGTH 7  // For the test lstm_model.pt model only
+
+/// @brief Measure the elaspsed time of the process of (input initilization + single round inference).
+/// @param input_shape the dimension of the input shape.
+/// @return duration of the process in milliseconds (10e-3 s).
+double benchmark(const std::vector<int64_t>& input_shape, bool use_gpu, phasm::TorchscriptModel& model) {
+    torch::Device bench_device = use_gpu ? torch::kCUDA : torch::kCPU;
+
+    auto t_start = std::chrono::high_resolution_clock::now();
+
+    std::vector<torch::jit::IValue> input;
+    input.push_back(torch::randn(input_shape, bench_device));
+
+    at::Tensor output = model.get_module().forward(input).toTensor();
+    // Operate on the first element of the output to make sure inference execution is completed.
+    // std::cout << output.slice(/*dim=*/0, /*start=*/0, /*end=*/1) << '\n';
+    float firstElement = output[0][0].item<float>();
+    firstElement += 1.0;
+
+    auto t_end = std::chrono::high_resolution_clock::now();
+    auto t_duration = std::chrono::duration_cast<std::chrono::microseconds>(t_end - t_start).count();
+
+    return double(t_duration) / 1000.0;  // return in milliseconds
+}
 
 int main(int argc, const char *argv[]) {
     if (argc != 2) {
@@ -27,12 +54,8 @@ int main(int argc, const char *argv[]) {
 
     phasm::get_libtorch_version();
 
-    bool has_gpu = phasm::has_cuda_device();
-    torch::Device device= torch::kCPU;
-    if (has_gpu) {
-        std::cout << "Use CUDA device 0 to load module!\n" << std::endl;
-        device = torch::kCUDA;
-    }
+    /** FIRST RD: demo functionality on CPU */
+    torch::Device device = torch::kCPU;
 
     /* Load module to the assigned device */
     phasm::TorchscriptModel model = phasm::TorchscriptModel(argv[1], true, device);
@@ -40,11 +63,11 @@ int main(int argc, const char *argv[]) {
     /* Get the input layer information and claim the input based on device */
     std::vector<int64_t> first_layer_shape = model.GetFirstLayerShape();
 
-    // Caculate the input dimension, based on first layer weight matrix shape.
+    // Caculate the input dimension, based on first layer's weight matrix shape.
     // TODO (@xmei): below method is for RNN layer only. Different for nn.linear and nn.conv.
     std::vector<int64_t> input_shape;
-    input_shape.push_back(BATCH_SIZE);
-    input_shape.push_back(SEQ_LENGTH);
+    input_shape.push_back(DEMO_BATCH_SIZE);
+    input_shape.push_back(DEMO_SEQ_LENGTH);
     std::copy(first_layer_shape.begin() + 1, first_layer_shape.end(), std::back_inserter(input_shape));
     std::cout << "Input dimension: " << input_shape << std::endl;
 
@@ -58,5 +81,24 @@ int main(int argc, const char *argv[]) {
     std::cout << "Output.device().type(): " << output.device().type() << std::endl;
     std::cout << output.slice(/*dim=*/0, /*start=*/0, /*end=*/5) << '\n';
 
+    /** BENCHMARKING ON CPU*/
+    std::cout << "\n\n########################################\n";
+    std::cout << "Benchmarking on CPU \n";
+    std::cout << "########################################\n";
+    std::cout << "batch_size,\ttime (milli-seconds)\n";
+    for (int64_t batch_size = MIN_BATCH_SIZE; batch_size <= MAX_BATCH_SIZE; batch_size *= 2) {
+        input_shape.clear();
+        input_shape.push_back(batch_size);
+        input_shape.push_back(DEMO_SEQ_LENGTH);
+        std::copy(first_layer_shape.begin() + 1, first_layer_shape.end(), std::back_inserter(input_shape));
+
+        std::cout << batch_size << ",\t" << benchmark(input_shape, false, model) << std::endl;
+    }
+
+    // bool has_gpu = phasm::has_cuda_device();
+    // if (has_gpu) {
+    //     std::cout << "Use CUDA device 0 to load module!\n" << std::endl;
+    //     device = torch::kCUDA;
+    // }
     return 0;
 }
